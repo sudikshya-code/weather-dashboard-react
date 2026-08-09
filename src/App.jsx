9fa44002402051535632a74a89796c2f04d667ff
Sudikshya Adhikari <asudikshya6206@gmail.com>
2026-08-09 21:25:36 +0545

    Fix import paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
+import Navbar from "./Components/Navbar";
 
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
 
 import "./App.css";
 
